f62429eb573d9f2130dc3af49b636c7cb2ea76af
Orlando <orlahdz50@gmail.com>
2023-12-18 20:38:21 -0600

    feat: account type form and some validations.

---
+++ ManejadorPresupuesto/Controllers/AccountTypesController.cs
@@ -0,0 +1,25 @@
+﻿using ManejadorPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejadorPresupuesto.Controllers
+{
+	public class AccountTypesController : Controller
+	{
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(AccountType accountType)
+		{
+
+			if (!ModelState.IsValid)
+			{
+				return View(accountType);
+			}
+
+			return View();
+		}
+	}
+}
---
+++ ManejadorPresupuesto/Models/AccountType.cs
@@ -0,0 +1,29 @@
+﻿using ManejadorPresupuesto.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejadorPresupuesto.Models
+{
+	public class AccountType //: IValidatableObject
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "El campo {0} es requerido")]
+		[FirstCapitalLetter]
+		public string Nombre { get; set; }
+		public int UsuarioId { get; set; }
+		public int Orden { get; set; }
+
+   //     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+   //     {
+			//if (Nombre != null && Nombre.Length > 0)
+			//{
+			//	var firstLetter = Nombre[0].ToString();
+
+			//	if (firstLetter != firstLetter.ToUpper())
+			//	{
+			//		yield return new ValidationResult("La primera letra debe ser mayúscula", 
+			//			new[] { nameof(Nombre) });
+			//	}
+			//}
+   //     }
+    }
+}
